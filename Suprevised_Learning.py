ce720e9b54b9c12deffaf31210f73ec84ab86f0d
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-16 11:59:17 +0100

    Code Update

--- Suprevised_Learning.py
+++ Suprevised_Learning.py
@@ -1,5 +1,11 @@
 import urllib
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
 
-# Basic Packgaes 
+# X being the factors to base the prediction on
+# Y being the factor to predict (prediction)
+
+# Basic Packages
