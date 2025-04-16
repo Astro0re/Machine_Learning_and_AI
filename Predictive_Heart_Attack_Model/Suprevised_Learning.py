1a0e42a4c259d3c24931713b1bb68914dc0cd97a
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-16 12:27:55 +0100

    Code Update

--- Predictive_Heart_Attack_Model/Suprevised_Learning.py
+++ Predictive_Heart_Attack_Model/Suprevised_Learning.py
@@ -1,11 +1,22 @@
 import urllib
+
+import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 
+# To use Heart Attack in Russia Data to create a predictive model
+
 # X being the factors to base the prediction on
+# X Factors = Heart Attack History, Cholesterol, Diet, Excercise Habits, Obesity and others
+
 # Y being the factor to predict (prediction)
 
 # Basic Packages
+
+Heart_Data =pandas.read_csv (heart_attack_russia.csv)
+
+X=Heart_Data["Obesity"] 
+Y= Heart_Data[]
