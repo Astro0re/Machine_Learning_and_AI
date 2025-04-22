78c9ea2a02c25d127a3c6616cd7b1552e23b6e34
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-22 18:43:59 +0100

    Code Update

--- Predictive_Heart_Attack_Model/Suprevised_Learning.py
+++ Predictive_Heart_Attack_Model/Suprevised_Learning.py
@@ -1,6 +1,8 @@
 # Basic Packages
 import urllib
-import statistics 
+import statistics
+from statistics import linear_regression
+
 import numpy as nu
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,11 +20,21 @@ from sklearn.linear_model import LinearRegression
 
 
 # Data Exploration
-Heart_Data =pd.read_csv ( C:\Users\USER\Documents\VSC\Git_\AI\Predictive_Heart_Attack_Model\heart_attack_russia.csv )
+Heart_Data =pd.read_csv ( r"C:\Users\USER\Documents\VSC\Git_\AI\Predictive_Heart_Attack_Model\heart_attack_russia.csv" )
 print(Heart_Data.info())
 print(Heart_Data.head())
-print(Heart_Data.index())
+print(Heart_Data.shape)
+# print(Heart_Data.index)
 
-X= Heart_Data["Obesity", "Heart-Attack-History","Cholesterol","Diet","Excercise-Habits"] 
+# X= Heart_Data["Obesity", "Heart-Attack-History","Cholesterol","Diet",]
+x= Heart_Data.loc[:, Heart_Data.columns != 'Heart-Attack']
 Y= Heart_Data["Heart-Attack"]
 
+
+kn=KNeighborsRegressor()
+kn.fit(x,Y)
+pre_1=kn.predict(x)
+
+lr=LinearRegression()
+lr.fit(x,Y)
+per_1= lr.predict(x)
