0e69d2163ab136b0919a3f47d233b5471bce82f8
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-16 16:24:55 +0100

    Logistic regression

--- logistic regression.py
+++ logistic regression.py
@@ -3,5 +3,18 @@
 # Packages 
 import pandas as pd
 import numpy as np 
-import matplotlib as plts
-import tensorflow as tf
+import matplotlib.pyplot as py
+from sklearn.linear_model import LogisticRegression
+
+df =pd.read_csv('iris.csv')
+
+x = df.drop(columns=['id','species'])
+y= df['species']
+
+model = LogisticRegression()
+
+model.fit(x,y)
+
+model.predict([[5.1,3.5,1.4,0.2]])
+
+py.scatter(df['sepal_length'],df['sepal_width'],c=df['species'])
